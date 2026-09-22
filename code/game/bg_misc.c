aa4aa3c5cfc249bc89099f173061d9b887731d06
noire.dev <noirecx@gmail.com>
2026-09-22 21:50:14 +0300

    item fix

--- code/game/bg_misc.c
+++ code/game/bg_misc.c
@@ -28,175 +28,18 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 item_t jsd_item[MAX_ITEMS];
 int jsd_itemCount = 0;
 
-#if 0
-item_t jsd_item[] = {{NULL,
-                          NULL,
-                          {NULL, NULL, NULL, NULL},
-                          /* icon */ NULL,
-                          /* pickup */ NULL,
-                          0,
-                          0,
-                          0,
-                          /* precache */ "",
-                          /* sounds */ ""},  // leave index 0 alone
-
-                         //
-                         // WEAPONS
-                         //
-
-                         /*QUAKED weapon_gauntlet (.3 .3 1) (-16 -16 -16) (16 16 16) suspended
-                          */
-                         {"weapon_gauntlet",
-                          "sound/misc/w_pkup.wav",
-                          {"models/weapons2/gauntlet/gauntlet.md3", NULL, NULL, NULL},
-                          /* icon */ "icons/iconw_gauntlet",
-                          /* pickup */ "Gauntlet",
-                          0,
-                          IT_WEAPON,
-                          WP_GAUNTLET,
-                          /* precache */ "",
-                          /* sounds */ ""},
-
-                         /*QUAKED weapon_shotgun (.3 .3 1) (-16 -16 -16) (16 16 16) suspended
-                          */
-                         {"weapon_shotgun",
-                          "sound/misc/w_pkup.wav",
-                          {"models/weapons2/shotgun/shotgun.md3", NULL, NULL, NULL},
-                          /* icon */ "icons/iconw_shotgun",
-                          /* pickup */ "Shotgun",
-                          10,
-                          IT_WEAPON,
-                          WP_SHOTGUN,
-                          /* precache */ "",
-                          /* sounds */ ""},
-
-                         /*QUAKED weapon_machinegun (.3 .3 1) (-16 -16 -16) (16 16 16) suspended
-                          */
-                         {"weapon_machinegun",
-                          "sound/misc/w_pkup.wav",
-                          {"models/weapons2/machinegun/machinegun.md3", NULL, NULL, NULL},
-                          /* icon */ "icons/iconw_machinegun",
-                          /* pickup */ "Machinegun",
-                          40,
-                          IT_WEAPON,
-                          WP_MACHINEGUN,
-                          /* precache */ "",
-                          /* sounds */ ""},
-
-                         /*QUAKED weapon_grenadelauncher (.3 .3 1) (-16 -16 -16) (16 16 16) suspended
-                          */
-                         {"weapon_grenadelauncher",
-                          "sound/misc/w_pkup.wav",
-                          {"models/weapons2/grenadel/grenadel.md3", NULL, NULL, NULL},
-                          /* icon */ "icons/iconw_grenade",
-                          /* pickup */ "Grenade Launcher",
-                          10,
-                          IT_WEAPON,
-                          WP_GRENADE_LAUNCHER,
-                          /* precache */ "",
-                          /* sounds */ "sound/weapons/grenade/hgrenb1a.wav sound/weapons/grenade/hgrenb2a.wav"},
-
-                         /*QUAKED weapon_rocketlauncher (.3 .3 1) (-16 -16 -16) (16 16 16) suspended
-                          */
-                         {"weapon_rocketlauncher",
-                          "sound/misc/w_pkup.wav",
-                          {"models/weapons2/rocketl/rocketl.md3", NULL, NULL, NULL},
-                          /* icon */ "icons/iconw_rocket",
-                          /* pickup */ "Rocket Launcher",
-                          10,
-                          IT_WEAPON,
-                          WP_ROCKET_LAUNCHER,
-                          /* precache */ "",
-                          /* sounds */ ""},
-
-                         /*QUAKED weapon_lightning (.3 .3 1) (-16 -16 -16) (16 16 16) suspended
-                          */
-                         {"weapon_lightning",
-                          "sound/misc/w_pkup.wav",
-                          {"models/weapons2/lightning/lightning.md3", NULL, NULL, NULL},
-                          /* icon */ "icons/iconw_lightning",
-                          /* pickup */ "Lightning Gun",
-                          100,
-                          IT_WEAPON,
-                          WP_LIGHTNING,
-                          /* precache */ "",
-                          /* sounds */ ""},
-
-                         /*QUAKED weapon_railgun (.3 .3 1) (-16 -16 -16) (16 16 16) suspended
-                          */
-                         {"weapon_railgun",
-                          "sound/misc/w_pkup.wav",
-                          {"models/weapons2/railgun/railgun.md3", NULL, NULL, NULL},
-                          /* icon */ "icons/iconw_railgun",
-                          /* pickup */ "Railgun",
-                          10,
-                          IT_WEAPON,
-                          WP_RAILGUN,
-                          /* precache */ "",
-                          /* sounds */ ""},
-
-                         /*QUAKED weapon_plasmagun (.3 .3 1) (-16 -16 -16) (16 16 16) suspended
-                          */
-                         {"weapon_plasmagun",
-                          "sound/misc/w_pkup.wav",
-                          {"models/weapons2/plasma/plasma.md3", NULL, NULL, NULL},
-                          /* icon */ "icons/iconw_plasma",
-                          /* pickup */ "Plasma Gun",
-                          50,
-                          IT_WEAPON,
-                          WP_PLASMAGUN,
-                          /* precache */ "",
-                          /* sounds */ ""},
-
-                         /*QUAKED weapon_bfg (.3 .3 1) (-16 -16 -16) (16 16 16) suspended
-                          */
-                         {"weapon_bfg",
-                          "sound/misc/w_pkup.wav",
-                          {"models/weapons2/bfg/bfg.md3", NULL, NULL, NULL},
-                          /* icon */ "icons/iconw_bfg",
-                          /* pickup */ "BFG10K",
-                          20,
-                          IT_WEAPON,
-                          WP_BFG,
-                          /* precache */ "",
-                          /* sounds */ ""},
-
-                         // end of list marker
-                         {NULL}};
-
-int jsd_itemCount = ARRAY_LEN(jsd_item) - 1;
-#endif
-
-/*
-===============
-BG_FindItemForWeapon
-
-===============
-*/
 item_t* BG_FindItemForWeapon(weapon_t weapon) {
-	item_t* it;
-
-	for(it = jsd_item + 1; it->classname; it++) {
-		if(it->giType == IT_WEAPON && it->giTag == weapon) {
-			return it;
-		}
+	for(int i = 0; i < jsd_itemCount; i++) {
+		if(jsd_item[i].giType == IT_WEAPON && jsd_item[i].giTag == weapon) return &jsd_item[i];
 	}
 
 	Com_Error(ERR_DROP, "Couldn't find item for weapon %i", weapon);
 	return NULL;
 }
 
-/*
-===============
-BG_FindItem
-
-===============
-*/
 item_t* BG_FindItem(const char* pickupName) {
-	item_t* it;
-
-	for(it = jsd_item + 1; it->classname; it++) {
-		if(!Q_stricmp(it->pickup_name, pickupName)) return it;
+	for(int i = 0; i < jsd_itemCount; i++) {
+		if(!Q_stricmp(jsd_item[i].pickup_name, pickupName)) return &jsd_item[i];
 	}
 
 	return NULL;
@@ -234,9 +77,7 @@ This needs to be the same for client side prediction and server use.
 bool BG_CanItemBeGrabbed(const entityState_t* ent, const playerState_t* ps) {
 	item_t* item;
 
-	if(ent->modelindex < 1 || ent->modelindex >= jsd_itemCount) {
-		Com_Error(ERR_DROP, "BG_CanItemBeGrabbed: index out of range");
-	}
+	if(ent->modelindex < 1 || ent->modelindex >= jsd_itemCount) Com_Error(ERR_DROP, "BG_CanItemBeGrabbed: index out of range");
 
 	item = &jsd_item[ent->modelindex];
 
